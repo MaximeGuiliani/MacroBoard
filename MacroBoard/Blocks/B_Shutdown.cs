97bf561c60216b810fd44f2c7ef188edaaaf00fd
Heba ABU RABIA <heba.ABU-RABIA@etu.univ-amu.fr>
2022-04-26 16:56:03 +0200

    Shutdown and restart

---
+++ MacroBoard/Blocks/B_Shutdown.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroBoard
+{
+    internal class B_Shutdown : Block
+    {
+        public B_Shutdown()
+        {
+        }
+
+        public override void Execute()
+        {
+            Process.Start("powershell.exe", "shutdown -s");
+
+
+
+
+        }
+    }
+}
+
