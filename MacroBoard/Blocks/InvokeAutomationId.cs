1b14eaca438af5224c074a487268f9c05768d5f1
MOUBARIK <zineb.moubarik@etu.univ-amu.fr>
2022-04-28 16:53:17 +0200

    + 2 blocks

---
+++ MacroBoard/Blocks/InvokeAutomationId.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace MacroBoard
+{
+    internal class InvokeAutomationId : Block
+    {
+        string automationID;
+        public InvokeAutomationId(string automationID)
+        {
+            this.automationID = automationID;   
+        }
+        public override void Execute()
+        {
+            [DllImport("user32.dll")]
+            static extern IntPtr GetActiveWindow();
+            AutomationElement elt_app = AutomationElement.FromHandle(GetActiveWindow());
+            AutomationElementCollection automationElement =  elt_app.FindAll(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, automationID));
+            foreach(AutomationElement ele in automationElement)
+            {
+                InvokePattern inv = ele.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+                inv.Invoke();
+                break;
+            }
+        }
+    }
+}
