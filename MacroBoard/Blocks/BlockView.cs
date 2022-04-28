78b1ab5bd9ef37303a6837c9a721800123d84279
Gazaix <damien.gazaix@etu.univ-amu.fr>
2022-04-28 12:17:05 +0200

    Création des BlockView (blocs dans une liste)

---
+++ MacroBoard/Blocks/BlockView.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Diagnostics;
+
+namespace MacroBoard
+{
+    /*
+     <Grid x:Name="MainGrid" Margin="5" Width="229">
+        <Label x:Name="Label_Name" Content="Test" Height="26" Margin="15,7,15,7" HorizontalAlignment="Center"/>
+        <Button x:Name="Button_Up" Width="15" Height="15" HorizontalAlignment="Left" VerticalAlignment="Top">
+            <TextBlock Text="🠕" Margin="0,-3,0,0"/>
+        </Button>
+        <Button x:Name="Button_Down" Width="15" Height="15" HorizontalAlignment="Left" VerticalAlignment="Bottom">
+            <TextBlock Text="🠗" Margin="0,-3,0,0" VerticalAlignment="Stretch"/>
+        </Button>
+        <Button x:Name="Button_Delete" Width="15" Height="11" HorizontalAlignment="Right" VerticalAlignment="Top">
+            <TextBlock Text="-" Margin="0,-6,0,0" VerticalAlignment="Stretch"/>
+        </Button>
+        <Button x:Name="Button_Edit" Width="30" Height="15" HorizontalAlignment="Right" VerticalAlignment="Bottom">
+            <TextBlock Text="edit" Margin="0,-3,0,0" VerticalAlignment="Stretch"/>
+        </Button>
+      </Grid>
+    */
+    internal class BlockView
+    {
+        public Button Btn_Delete { get; } = new();
+        public Button Btn_Edit { get; } = new();
+        public Button Btn_Up { get; } = new();
+        public Button Btn_Down { get; } = new ();
+        private Label lbl_Name = new();
+        public Grid Content { get; } = new();
+
+        public BlockView(string Name)
+        {
+            Setup_Btns();
+            Setup_Name(Name);
+
+            Thickness thickness = new Thickness();
+            thickness.Left = 5d;
+            thickness.Right = 5d;
+            thickness.Top = 5d;
+            thickness.Bottom = 5d;
+            Content.Margin = thickness;
+            Content.Width = 230d;
+
+            Content.Children.Add(lbl_Name);
+            Content.Children.Add(Btn_Delete);
+        }
+
+        private void Setup_Name(string Name)
+        {
+            lbl_Name.Content = Name;
+            lbl_Name.HorizontalAlignment = HorizontalAlignment.Center;
+        }
+
+        private void Setup_Btns()
+        {
+            TextBlock txtBlock = new();
+            txtBlock.Text = "-";
+            Thickness thickness = new Thickness();
+            thickness.Top = -6d;
+            txtBlock.Margin = thickness;
+            Btn_Delete.Content = txtBlock;
+
+            Btn_Delete.Width = 15;
+            Btn_Delete.Height = 11;
+            Btn_Delete.HorizontalAlignment = HorizontalAlignment.Right;
+            Btn_Delete.VerticalAlignment = VerticalAlignment.Top;
+        }
+
+    }
+}
--- MacroBoard/Blocks/WindowTEST.xaml.cs
+++ MacroBoard/Blocks/WindowTEST.xaml.cs
@@ -20,72 +20,33 @@ namespace MacroBoard
     /// </summary>
     public partial class WindowTEST : Window
     {
-        private List<Frame> BlocksFrame = new();
-        
+        private List<BlockView> BlockViews = new();
+
         public WindowTEST()
         {
             InitializeComponent();
-            InitBlock();   
+            InitBlock();
         }
 
         private void InitBlock()
         {
-            List<Grid> grid = new();
-            grid.Add(new Grid());
-            grid.Add(new Grid());
+            BlockViews.Add(new BlockView("Restart Computer"));
+            BlockViews.Add(new BlockView("Run App"));
 
+            foreach(BlockView blockView in BlockViews)
+            {
+                blockView.Btn_Delete.Click += OnClick_Delete;
+                listTest.Items.Add(blockView.Content);
+            }
 
-            Button btn = new Button();
-            btn.Content = "Bouton de fou la 1";
-            btn.Click += test;
-            grid[0].Children.Add(btn);
-
-            Button btn2 = new Button();
-            btn2.Content = "Bouton de fou la 2";
-            btn2.Click += test;
-            grid[1].Children.Add(btn2);
-
-
-            listTest.Items.Add(grid[0]);
-            listTest.Items.Add(grid[1]);
         }
 
-        private void test(object sender, RoutedEventArgs e)
+        private void OnClick_Delete(object sender, RoutedEventArgs e)
         {
-            Trace.WriteLine("dkhgshkfjsfvjkdhs");
+            listTest.Items.Remove(((Button)sender).Parent);
         }
 
-        private void OnClick(object sender, RoutedEventArgs e)
-        {
-            //   Block testBlock = new Blocks.B_CreateTextFile("test", "test2", "info", @"\\Mac\Home\Desktop\", "test", ".exe", "Bonsoir ceci est un text heba");
-            //   testBlock.Execute();
-            listTest.Items.Add("hello");
-            
-            listTest.Items.Add(new Button());
-            
-        }
 
-        private void OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            mainButton.Background = new SolidColorBrush(Colors.Red);
-        }
-
-        private void OnMouseLeave(object sender, MouseEventArgs e)
-        {
-            mainButton.Background = new SolidColorBrush(Colors.Blue);
-
-        }
     }
 
-    public class BlockView
-    {
-        private string Label_Name { get; }
-        private string Button_Delete { get; }
-
-        public BlockView(string Block_Name, string DeleteButton_Name)
-        {
-            this.Label_Name = Block_Name;
-            this.Button_Delete = DeleteButton_Name;
-        }
-    }
 }
