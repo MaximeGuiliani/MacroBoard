ea8d352d581a5ee7fd3739544cc98b858453a28f
Léo-Paul <93158921+LPLPM@users.noreply.github.com>
2022-04-29 13:57:25 +0200

    ajout du block ClickD

---
+++ MacroBoard/Blocks/ClickD.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MacroBoard
+{
+    internal class ClickD : Block
+    {
+
+        
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LeftDown = 0x00000002,
+            LeftUp = 0x00000004,
+            MiddleDown = 0x00000020,
+            MiddleUp = 0x00000040,
+            Move = 0x00000001,
+            Absolute = 0x00008000,
+            RightDown = 0x00000008,
+            RightUp = 0x00000010
+        }
+
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        public override void Execute()
+        {
+            mouse_event
+                ((int)MouseEventFlags.RightDown,
+                 0,
+                 0,
+                 0,
+                 0)
+                ;
+
+            mouse_event
+                ((int)MouseEventFlags.RightUp,
+                 0,
+                 0,
+                 0,
+                 0)
+                ;
+        }
+
+
+
+
+    }
+
+}
+
+
+
