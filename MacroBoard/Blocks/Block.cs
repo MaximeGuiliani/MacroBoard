2b2bc2d97210f641a38f316fa14dbf997d947415
Léo-Paul <93158921+LPLPM@users.noreply.github.com>
2022-04-29 12:43:00 +0200

    ajout de l'attribut BlockType

--- MacroBoard/Blocks/Block.cs
+++ MacroBoard/Blocks/Block.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,13 @@ using System.Windows.Controls;
 
 namespace MacroBoard{
     public abstract class Block{
+        
+		public Block()
+        {
+            this.BlockType = this.GetType().Name;
+        }
 
+        public string BlockType { get; set; } = "";
         public string Name { get; set; } = "";
         public string LogoUrl { get; set; } = "";
         public string info { get; set; } = "";
