5de445418d3aed1cc7a7d3a95a017b2bd17914a5
Léo-Paul <93158921+LPLPM@users.noreply.github.com>
2022-05-03 11:02:21 +0200

    update avec openCV

--- MacroBoard/Blocks/Recognition.cs
+++ MacroBoard/Blocks/Recognition.cs
@@ -1,190 +1,141 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Diagnostics;
-using System.Threading;
-using System.Runtime.InteropServices;
-using System.Net;
 using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
 using System.Drawing.Imaging;
-using System.Drawing;
-using Image = System.Drawing.Image;
-using System.IO;
+using System.Threading;
+
 
 namespace MacroBoard
 {
     internal class Recognition : Block
     {
 
-        int screenNumber;
-        string imagePath;
-        int offSetX;
-        int offSetY;
+        int    widthScreen;
+        int    heightScreen; 
+        string templatePath;
+        Rect   rectOfInterest;
+        int    offSetX, offSetY;
+        IEnumerable<double> tryScales;
+        bool   debugMode;
+        string debugDirPath;
+        TemplateMatchModes matchModes;
 
-        public Recognition(string ImagePath, int offSetX=0, int offSetY=0, int screenNumber=0)
+
+        public Recognition( string templatePath,
+                      int xInterest=0, int yInterest=0, int heightInterest=0, int widthInterest=0,
+                      int screenNumber=0,
+                      int offSetX=0, int offSetY=0,
+                      double scale=1, bool loop=false,
+                      bool debugMode=false,
+                      TemplateMatchModes matchModes=TemplateMatchModes.CCoeffNormed)
         {
-            this.imagePath = ImagePath;
-            this.offSetX = offSetX;
-            this.offSetY = offSetY;
-            this.screenNumber = screenNumber;
+            this.widthScreen    = Screen.AllScreens[screenNumber].Bounds.Width;
+            this.heightScreen   = Screen.AllScreens[screenNumber].Bounds.Height;
+            this.templatePath   = templatePath;
+            this.rectOfInterest = new Rect(xInterest, yInterest, widthInterest==0? this.widthScreen-xInterest:widthInterest, heightInterest==0? this.heightScreen-yInterest:heightInterest);            
+            this.offSetX        = offSetX;
+            this.offSetY        = offSetY;
+            int[] pourcentages  = {100, 125, 150, 175, 200};
+            var scales = (from p1 in pourcentages from p2 in pourcentages where p1!=p2 select (double)p1/(double)p2).Append(1);
+            this.tryScales      = (loop)? scales:new double[]{scale};
+            this.matchModes     = matchModes;
+            this.debugMode      = debugMode ;
+            this.debugDirPath   = $@"C:\Users\{Environment.GetEnvironmentVariable("USERNAME")}\Documents\";
         }
 
-        private Rectangle searchBitmap(Bitmap smallBmp, Bitmap bigBmp, double tolerance)
+
+        //NOTE: les valeurs affichées correspondent au nombre de pixels sans l'échelonnage appliqué (ex:150%)
+        public override void Execute()
         {
-            BitmapData smallData =
-              smallBmp.LockBits(new Rectangle(0, 0, smallBmp.Width, smallBmp.Height),
-                       System.Drawing.Imaging.ImageLockMode.ReadOnly,
-                       System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-            BitmapData bigData =
-              bigBmp.LockBits(new Rectangle(0, 0, bigBmp.Width, bigBmp.Height),
-                       System.Drawing.Imaging.ImageLockMode.ReadOnly,
-                       System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            Mat image    = (debugMode)? new Mat(FileScreenShot()) : BitmapConverter.ToMat(BitmapScreenShot());
+            Mat template = new Mat(templatePath);
 
-            int smallStride = smallData.Stride; //décalage +1 ligne (1 Stride = 3*largeur car rgb +offset )
-            int bigStride = bigData.Stride;
+            (double maxVal, OpenCvSharp.Point? maxLoc, double scale) found = find(image, template);
 
-            int bigWidth = bigBmp.Width;
-            int bigHeight = bigBmp.Height - smallBmp.Height + 1;
-            int smallWidth = smallBmp.Width * 3;
-            int smallHeight = smallBmp.Height;
 
-            System.Drawing.Rectangle location = Rectangle.Empty;
-            int margin = Convert.ToInt32(255.0 * tolerance);
+            OpenCvSharp.Point rectPt1 = new OpenCvSharp.Point( (found.maxLoc?.X * ((0 < found.scale && found.scale < 1) ? (1 / found.scale) : 1) + rectOfInterest.X).GetValueOrDefault(),
+                                                               (found.maxLoc?.Y * ((0 < found.scale && found.scale < 1) ? (1 / found.scale) : 1) + rectOfInterest.Y).GetValueOrDefault() );
+            OpenCvSharp.Point rectPt2 = new OpenCvSharp.Point(rectPt1.X + ((1 < found.scale) ? template.Width / found.scale : template.Width),
+                                                              rectPt1.Y + ((1 < found.scale) ? template.Height / found.scale : template.Height));
+            Cv2.Rectangle(image, rectPt1, rectPt2, Scalar.Red, 1);
 
-            unsafe
+
+            OpenCvSharp.Point CirclePt = new OpenCvSharp.Point( rectPt1.X + ( (1<found.scale)? template.Width/found.scale:template.Width )/2 + offSetX,
+                                                                rectPt1.Y + ( (1<found.scale)? template.Height/found.scale:template.Height ) /2 + offSetY );
+            Cv2.Circle(image, CirclePt, 1, Scalar.Purple, 5);
+            SetCursorPos(CirclePt.X, CirclePt.Y);
+
+
+            if (debugMode) image.SaveImage(this.debugDirPath + @$"macroboard_{GetType().Name}_screenshot_annotated.png");
+            //Cv2.ImShow("resizedImageCrop", resizedImageCrop);
+            //Cv2.ImShow("resizedTemplate", resizedTemplate);
+            //Cv2.ImShow("image", image);
+        }
+
+
+
+        private (double, OpenCvSharp.Point?, double) find(Mat image, Mat template)
+        {
+            Mat imageCrop = image[this.rectOfInterest];
+
+            (double maxVal, OpenCvSharp.Point? maxLoc, double scale) found = (-1, null, -1);
+            Mat resizedImageCrop = new Mat();
+            Mat resizedTemplate = new Mat();
+            Mat result;
+            foreach (double scale in tryScales)
             {
-                byte* pSmall = (byte*)(void*)smallData.Scan0; //pointeur sur premiere couleur de 1er pixel
-                byte* pBig = (byte*)(void*)bigData.Scan0;
-
-                int smallOffset = smallStride - smallBmp.Width * 3;
-                int bigOffset = bigStride - bigBmp.Width * 3;
-
-                bool matchFound = true;
-
-                for (int y = 0; y < bigHeight; y++) //x,y,i,j utile pour éviter les offset et pour le return 
+                if (0 < scale && scale < 1)
                 {
-                    for (int x = 0; x < bigWidth; x++)
-                    {
-                        byte* pBigBackup = pBig;
-                        byte* pSmallBackup = pSmall;
-
-                        //Look for the small picture.
-                        for (int i = 0; i < smallHeight; i++)
-                        {
-                            int j = 0;
-                            matchFound = true;
-                            for (j = 0; j < smallWidth; j++)
-                            {
-                                //With tolerance: pSmall value should be between margins.
-                                int inf = pBig[0] - margin;
-                                int sup = pBig[0] + margin;
-                                if (sup < pSmall[0] || inf > pSmall[0])
-                                {
-                                    matchFound = false;
-                                    break;
-                                }
-
-                                pBig++;
-                                pSmall++;
-                            }
-
-                            if (!matchFound) break;
-
-                            //We restore the pointers. (comme au début de la ligne)
-                            pSmall = pSmallBackup;
-                            pBig = pBigBackup;
-
-                            //Next rows of the small and big pictures.
-                            pSmall += smallStride * (1 + i);
-                            pBig += bigStride * (1 + i);
-                        }
-
-                        //If match found, we return.
-                        if (matchFound)
-                        {
-                            location.X = x;
-                            location.Y = y;
-                            location.Width = smallBmp.Width;
-                            location.Height = smallBmp.Height;
-                            break;
-                        }
-                        //If no match found, we restore the pointers and continue.
-                        else
-                        {
-                            pBig = pBigBackup;
-                            pSmall = pSmallBackup;
-                            pBig += 3;
-                        }
-                    }
-                    //ici fin d'une ligne big 
-                    if (matchFound) break;
-
-                    pBig += bigOffset;
+                    Cv2.Resize(imageCrop, resizedImageCrop, new OpenCvSharp.Size(imageCrop.Width * scale, imageCrop.Height * scale));
+                    result = resizedImageCrop.MatchTemplate(template, this.matchModes);
                 }
+                else if (1 < scale)
+                {
+                    //MessageBox.Show($"wo:{template.Width} w:{template.Width / scale}  ho:{template.Height}  h:{template.Height / scale}  scale: {scale}");
+                    Cv2.Resize(template, resizedTemplate, new OpenCvSharp.Size(template.Width / scale, template.Height / scale));
+                    result = imageCrop.MatchTemplate(resizedTemplate, this.matchModes);
+                }
+                else
+                {
+                    result = imageCrop.MatchTemplate(template, this.matchModes);
+                }
+
+                double minVal, maxVal;
+                OpenCvSharp.Point minLoc, maxLoc;
+                result.MinMaxLoc(out minVal, out maxVal, out minLoc, out maxLoc);
+
+                if (found.scale < 0 || found.maxVal < maxVal) found = (maxVal, maxLoc, scale);
             }
+            return found;
+        }
 
-            bigBmp.UnlockBits(bigData);
-            smallBmp.UnlockBits(smallData);
 
-            return location;
+        private Bitmap BitmapScreenShot()
+        {
+            Bitmap screenShotBitmap = new Bitmap(widthScreen, heightScreen, PixelFormat.Format32bppRgb );
+            Graphics screenShotGraphics = Graphics.FromImage(screenShotBitmap);
+            screenShotGraphics.CopyFromScreen(0, 0, 0, 0, new System.Drawing.Size(widthScreen, heightScreen));
+            return screenShotBitmap;
+        }
+
+
+        private string FileScreenShot()
+        {
+            string filePath = this.debugDirPath + @$"macroboard_{GetType().Name}_screenshot.png";
+            BitmapScreenShot().Save(filePath);
+            return filePath;
         }
 
 
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         static extern bool SetCursorPos(int x, int y);
 
-
-        private Bitmap capture(){
-            int width = Screen.AllScreens[this.screenNumber].Bounds.Width;
-            int height = Screen.AllScreens[this.screenNumber].Bounds.Height;
-            Bitmap captureBitmap = new Bitmap(width, height);
-            Graphics captureGraphics = Graphics.FromImage(captureBitmap);
-            captureGraphics.CopyFromScreen(0, 0, 0, 0, new System.Drawing.Size(width, height));
-            return captureBitmap;
-        }
-
-
-        public override void Execute()
-        {
-            Bitmap bitmap1 = new Bitmap(this.imagePath);
-            Bitmap bitmap2 = capture();
-            double tolerance;
-            bool found = false;
-            for (tolerance = 0; tolerance < 1; tolerance += 0.04)
-            {
-                Rectangle location = searchBitmap(bitmap1, bitmap2, tolerance);
-                if (location.Width != 0)
-                {
-                    found = true;
-                    SetCursorPos(location.X+(location.Width/2)+this.offSetX, location.Y+(location.Height/2)+this.offSetY);
-                    //MessageBox.Show($"Tolerance:{tolerance}    w:{location.Width}    h:{location.Height}    X: {location.X}   Y: {location.Y}");
-                    break;
-                }
-            }
-            if (!found)
-            {
-                //MessageBox.Show("pas trouvé :(");
-            }
-        }
-
-
-
-        /*******************************************************************************
-         * 
-         * TODO: réduire la zone de recherche si possible avec: pt_up_left pt_down_right
-         * TODO: ajouter "scaling" ou cas ou un scaling est utilisé par l'OS ?
-         * 
-         ******************************************************************************/
 
 
 
@@ -193,3 +144,6 @@ namespace MacroBoard
 
     }
 }
+
+
+//TODO: check rect of interest ne dépasse pas de l'image
