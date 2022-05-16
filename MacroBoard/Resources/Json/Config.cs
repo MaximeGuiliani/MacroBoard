bcfa4864ad71c9ba7c84b93cfe7299b2a0c33e16
Léo-Paul <lpmusardo@gmail.com>
2022-05-16 11:23:20 +0200

    ajout fichier config json

---
+++ MacroBoard/Resources/Json/Config.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+
+namespace MacroBoard
+{
+    class Config
+    {
+
+        const string relPathConfigFile = "./Resources/config/config.json";
+
+
+        public static string PathFromList(string configId)
+        {
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            JArray? paths = (JArray?)JObject.Parse(File.ReadAllText(relPathConfigFile))[configId];
+            foreach (string path in paths)
+            {
+                if (File.Exists(path) || new Regex("^[a-zA-Z0-9-_]+\\.exe$").IsMatch(path))
+                {
+                    return path.Replace("$USERNAME", Environment.GetEnvironmentVariable("USERNAME"));
+                }
+            }
+            return "";
+        }
+
+
+        public static string PathFromString(string configId)
+        {
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            return ((string)JObject.Parse(File.ReadAllText(relPathConfigFile))[configId]).Replace("$USERNAME", Environment.GetEnvironmentVariable("USERNAME"));
+        }
+
+
+
+
+    }
+}
+//TODO check la regex
+
