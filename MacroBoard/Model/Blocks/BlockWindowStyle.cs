dfd0ea2282f88148080d4eb42a3723139c4bb7d2
Léo-Paul <lpmusardo@gmail.com>
2022-05-18 16:17:23 +0200

    nouveau block window style

---
+++ MacroBoard/Model/Blocks/BlockWindowStyle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MacroBoard.Utils;
+
+
+namespace MacroBoard
+{
+    [Serializable]
+    public class BlockWindowStyle : Block
+    {
+        public WindowStyle windowStyle;
+
+        public BlockWindowStyle(WindowStyle windowStyle = WindowStyle.Normal)
+        {
+            base.Name = "Window Style";
+            base.info = "Set the style of the foreground window";
+            this.windowStyle = windowStyle;
+        }
+
+
+        public override void Execute()
+        {
+            ShowWindow(GetForegroundWindow(), (int)windowStyle);
+        }
+
+
+        public override void Accept(IBlockVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+
+
+    }
+}
