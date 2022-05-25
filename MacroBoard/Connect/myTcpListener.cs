ab1e0d58586154ef2d27f84bc066dfa0166d6c40
maxime <maxime.guiliani@hotmail.fr>
2022-05-25 18:01:18 +0200

    1v1 damien

--- MacroBoard/Connect/myTcpListener.cs
+++ MacroBoard/Connect/myTcpListener.cs
@@ -6,26 +6,30 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
 
 class myTcpListener
 {
     public myTcpListener()
     {
         Thread newThread = new Thread(new ThreadStart(Run));
+        newThread.SetApartmentState(ApartmentState.STA);
         newThread.Start();
     }
 
     public void Run()
     {
+
         TcpListener server = null;
         try
         {
             // Set the TcpListener on port 13000.
             Int32 port = 13000;
-            IPAddress localAddr = IPAddress.Parse("172.23.151.204");
+            // IPAddress localAddr = IPAddress.Parse("192.168.43.63");
 
             // TcpListener server = new TcpListener(port);
-            server = new TcpListener(localAddr, port);
+            server = new TcpListener(port);
 
             // Start listening for client requests.
             server.Start();
@@ -37,18 +41,17 @@ class myTcpListener
             // Enter the listening loop.
             while (true)
             {
-                Console.Write("Waiting for a connection... ");
+                Trace.Write("Waiting for a connection... ");
 
                 // Perform a blocking call to accept requests.
                 // You could also use server.AcceptSocket() here.
                 TcpClient client = server.AcceptTcpClient();
-                Console.WriteLine("Connected!");
+                Trace.WriteLine("Connected!");
                 NetworkStream stream = client.GetStream();
+                InitMobileData(stream);
+                //Console.WriteLine(InitMobileData().Count);
 
-                foreach (String s in InitMobileData())
-                {
-                    SendData(s, stream);
-                }
+
                 data = null;
 
                 // Get a stream object for reading and writing
@@ -61,12 +64,10 @@ class myTcpListener
                 {
                     // Translate data bytes to a ASCII string.
                     data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("Received: {0}", data);
+                    Trace.WriteLine("Received: {0}", data);
 
                     // Process the data sent by the client.
-                    data = data.ToUpper();
-
-                    SendData(data, stream);
+                    ExecuteGivenWorkflow(data.ToString());
                 }
 
                 // Shutdown and end connection
@@ -83,30 +84,72 @@ class myTcpListener
             server.Stop();
         }
 
-        Console.WriteLine("\nHit enter to continue...");
+        Trace.WriteLine("\nHit enter to continue...");
         Console.Read();
     }
 
-
-    public void  SendData(String data, NetworkStream stream)
+    private void ExecuteGivenWorkflow(String Name)
     {
-        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
-        // Send back a response.
+
+    }
+
+    public void InitMobileData(NetworkStream stream)
+    {
+        List<WorkflowView> lw = Serialization.getFavsFromJson();
+        List<string> lds = new();
+        byte[] msg = null;
+        foreach (WorkflowView wf in lw)
+        {
+            // Send back a response.
+            lds.Add(wf.CurrentworkFlow.imagePath);
+            lds.Add(wf.CurrentworkFlow.workflowName);
+
+            //msg = System.Text.Encoding.ASCII.GetBytes(wf.CurrentworkFlow.workflowName + "\n");
+
+            //Image image = Image.FromFile(wf.CurrentworkFlow.imagePath);
+
+
+            Bitmap tImage = new Bitmap(wf.CurrentworkFlow.imagePath);
+
+            byte[] test = (System.Text.Encoding.ASCII.GetBytes("<img>"));
+            // byte[] bStream = Encoding.ASCII.GetBytes(tImage.ToString());
+
+            byte[] bStream = ImageToByteArray(tImage);
+            byte[] test2 = System.Text.Encoding.ASCII.GetBytes("</img>");
+            byte[] saut = System.Text.Encoding.ASCII.GetBytes("\n");
+
+            byte[] all = new byte[test.Length + test2.Length + bStream.Length + 2];
+
+            test.CopyTo(all, 0);
+            saut.CopyTo(all, test.Length);
+            bStream.CopyTo(all, test.Length + 1);
+            saut.CopyTo(all, bStream.Length + test.Length + 1);
+            test2.CopyTo(all, bStream.Length + test.Length + 2);
+            saut.CopyTo(all, test2.Length+ bStream.Length + test.Length + 1);
+
+
+
+
+
+            stream.Write(all, 0, all.Length);
+
+
+
+        }
+
+        msg = System.Text.Encoding.ASCII.GetBytes("|");
         stream.Write(msg, 0, msg.Length);
+
     }
 
 
-    public List<string> InitMobileData()
+    public byte[] ImageToByteArray(Image imageIn)
     {
-        List<WorkflowView> lw =  Serialization.getWorkFlowsFromJson();
-        List<string> lds = new();
-        foreach(WorkflowView wf in lw)
+        using (var ms = new MemoryStream())
         {
-            lds.Add(wf.CurrentworkFlow.imagePath);
-            lds.Add(wf.CurrentworkFlow.workflowName);
+            imageIn.Save(ms, imageIn.RawFormat);
+            return ms.ToArray();
         }
-
-        return lds;
     }
 }
 
