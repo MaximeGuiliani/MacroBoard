75e17adfbc79f396ba9c70c607d3880dd93a171b
Léo-Paul <lpmusardo@gmail.com>
2022-05-30 10:47:53 +0200

    logo et ameliorations

--- MacroBoard/Model/Blocks/BlockInvokeAutomationId.cs
+++ MacroBoard/Model/Blocks/BlockInvokeAutomationId.cs
@@ -14,11 +14,11 @@ namespace MacroBoard
 
         public BlockInvokeAutomationId(string automationID)
         {
-            base.Name = "Block Invoke Automation Id";
-            base.info = "Invoke the AutomationElement on the foreground window.";
-            this.automationID = automationID;
+            base.Name = "Invoke AutomationId";
+            base.info = "Use the Invoke Pattern on the UIElement from the foreground window.\nTips: Look at the Pattern Support";
             base.category = Categories.Applications;
-
+            base.LogoUrl = "/Resources/Logo_Blocks/Logo_BlockInvoke.png";
+            this.automationID = automationID;
         }
 
 
@@ -30,10 +30,12 @@ namespace MacroBoard
             foreach (AutomationElement elt in elements)
             {
                 InvokePattern pattern = elt.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+                if (pattern == null) continue;
                 pattern.Invoke();
-                break;
+                break; // <=> on invoque un seul sur les trouvés
             }
         }
+
 
         public override void Accept(IBlockVisitor visitor)
         {
