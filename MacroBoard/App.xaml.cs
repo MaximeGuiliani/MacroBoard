e8a311849a9c7dac4f51d6c8468e58b2a75a4602
Léo-Paul <lpmusardo@gmail.com>
2022-06-03 10:45:59 +0200

    lecture stdin pour lancer l'app

--- MacroBoard/App.xaml.cs
+++ MacroBoard/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MacroBoard.Model;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -6,12 +7,53 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+
 namespace MacroBoard
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+
+        string[] args;
+
+        public void app_startup(object sender, StartupEventArgs e)
+        {
+            this.args = e.Args;
+            if (e.Args.Length == 1)
+                startUpConsole();
+            else
+                startUpWindow();
+        }
+
+
+        private void startUpWindow()
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+
+
+        private void startUpConsole()
+        {
+            try
+            {
+                //string workFlowName = args[0];
+                // string workFlowPath = AppDomain.CurrentDomain.BaseDirectory + @"\Resources\WFJSON\" + workFlowName + ".json";
+                string workFlowPath = args[0];
+                WorkFlow workFlow = new Serialization(workFlowPath).Deserialize();
+                Executor executor = new Executor(workFlow);
+                executor.Execute();
+                Application.Current.Shutdown();
+            }
+            catch (Exception ex)
+            {
+                MainWindow mw = new MainWindow("error occured with input");
+                mw.Show();
+            }
+        }
+
+
+
+
+
     }
 }
